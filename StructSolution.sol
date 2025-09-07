151b3870e234154f09387ffe40d9af63b6c447d1
Namoj-design <namojperiakumar@gmail.com>
2025-09-07 19:38:35 +0530

    structure

---
+++ StructSolution.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+contract learnStructs {
+    
+ struct Movie {
+     
+     string title;
+     string director;
+     uint movie_id;
+ }
+
+    Movie movie;
+    Movie comedy;
+    
+
+    function setMovie() public {
+       comedy = Movie('Zoolander', 'Ben Stiller', 4);
+    }
+
+    function getMovieId() public view returns(uint) {
+        return comedy.movie_id;
+    }
+
+}
